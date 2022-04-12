78d1f83bd270ee61a3bd223eee48e3811e87f1cb
Gerardo Padilla <gpadil@gerardos-mbp.lan>
2022-04-11 20:03:36 -0600

    getting user input and echoing

---
+++ pages/imgChangeOnMouseOver.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ImageChangeMouseOver = () => {
+    return (
+        <div>
+            <img src="" alt="" />
+            &nbsp;&nbsp;&ncsp;
+            <img src="" alt="" />
+        </div>
+    )
+}
+
+
+export default ImageChangeMouseOver
